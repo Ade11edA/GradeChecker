b927765a7a7352ea6ef8b359de89898ffd1d6aec
Adel Benchemam <student.178420@worcesterschools.net>
2023-12-05 16:05:56 -0500

    Added average calculator

--- grade-Calc.js
+++ grade-Calc.js
@@ -1,5 +1,5 @@
 document.querySelector('form').addEventListener('submit', checkGrade);
-
+const grades = [];
 function checkGrade(e){
     e.preventDefault();
     let grade = document.querySelector('input').value;
@@ -7,20 +7,29 @@ function checkGrade(e){
 
     console.log("If your grade is " + grade + " then...")
     if (grade >= 90 ) {
-        message += `<li>your grade is an A</li>`;
+        message += `<li>your grade of `+ grade + ` is an A</li>`;
+        grades.push(parseFloat(grade));
     }
     if (grade >= 80 && grade < 90) {
-        message += `<li>your grade is a B</li>`;
+        message += `<li>your grade of `+ grade + `is a B</li>`;
+        grades.push(parseFloat(grade));
     }
     if (grade >= 70 && grade < 80) {
-        message += `<li>your grade is a C</li>`;
+        message += `<li>your grade of `+ grade + ` is a C</li>`;
+        grades.push(parseFloat(grade));
     }
     if (grade >= 60 && grade < 70){
-        message +=`<li>your grade is a D</li>`;
+        message +=`<li>your grade of `+ grade + ` is a D</li>`;
+        grades.push(parseFloat(grade));
     }
     if (grade >= 0 && grade < 60){
-        message +=`<li>your grade is a F</li>`;
+        message +=`<li>your grade of `+ grade +` is a F</li>`;
+        grades.push(parseFloat(grade));
     }
     message += `</ul>`
-    document.querySelector('#output').innerHTML = message;
+    var sum = grades.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue
+      },0);      
+    document.querySelector('#output').innerHTML += message;
+    document.querySelector('#average').innerHTML = `Your average is ` + sum / grades.length;
 }
